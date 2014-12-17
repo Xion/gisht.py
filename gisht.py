83a4b110325d3fa5b4be1be1b0198b473e23bb77
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2014-12-17 01:04:54 +0100

    Skeleton of a Python package

---
+++ gisht.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+gisht
+"""
+__version__ = "0.0.1"
+__description__ = "Gists in the shell"
+__author__ = "Karol Kuczmarski"
+__license__ = "Simplified BSD"
+
+
+import sys
+
+
+def main(argv=sys.argv):
+    pass
+
+
+if __name__ == '__main__':
+    sys.exit(main() or 0)
---
+++ setup.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+"""
+gisht
+=====
+
+{description}
+"""
+import ast
+import os
+from setuptools import find_packages, setup
+import sys
+
+
+# Utility functions
+
+def read_tags(filename):
+    """Reads values of "magic tags" defined in the given Python file.
+
+    :param filename: Python filename to read the tags from
+    :return: Dictionary of tags
+    """
+    with open(filename) as f:
+        ast_tree = ast.parse(f.read(), filename)
+
+    res = {}
+    for node in ast.walk(ast_tree):
+        if type(node) is not ast.Assign:
+            continue
+
+        target = node.targets[0]
+        if type(target) is not ast.Name:
+            continue
+
+        if not (target.id.startswith('__') and target.id.endswith('__')):
+            continue
+
+        name = target.id[2:-2]
+        res[name] = ast.literal_eval(node.value)
+
+    return res
+
+
+def read_requirements(filename='requirements.txt'):
+    """Reads the list of requirements from given file.
+
+    :param filename: Filename to read the requirements from.
+                     Uses ``'requirements.txt'`` by default.
+
+    :return: Requirements as list of strings
+    """
+    # allow for some leeway with the argument
+    if not filename.startswith('requirements'):
+        filename = 'requirements-' + filename
+    if not os.path.splitext(filename)[1]:
+        filename += '.txt'  # no extension, add default
+
+    def valid_line(line):
+        line = line.strip()
+        return line and not any(line.startswith(p) for p in ('#', '-'))
+
+    def extract_requirement(line):
+        egg_eq = '#egg='
+        if egg_eq in line:
+            _, requirement = line.split(egg_eq, 1)
+            return requirement
+        return line
+
+    with open(filename) as f:
+        lines = f.readlines()
+        return list(map(extract_requirement, filter(valid_line, lines)))
+
+
+# setup() call
+
+SCRIPT = 'gisht.py'
+
+install_requires = read_requirements()
+if sys.version_info < (3, 0):
+    install_requires.extend(read_requirements('py2'))
+
+tags = read_tags(SCRIPT)
+__doc__ = __doc__.format(**tags)
+
+setup(
+    name='gisht',
+    version=tags['version'],
+    description=tags['description'],
+    long_description=__doc__,
+    author=tags['author'],
+    url="http://github.com/Xion/gisht",
+    license=tags['license'],
+
+    platforms='posix',
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS",
+        "Operating System :: Unix",
+        "Programming Language :: Python",
+        "Topic :: System :: Software Distribution",
+        "Topic :: Utilities",
+    ],
+
+    scripts=[SCRIPT],
+    entry_points={
+        'console_scripts': ['gisht=gisht:main'],
+    },
+
+    install_requires=install_requires,
+    tests_require=read_requirements('test'),
+)
