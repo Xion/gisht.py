3fe01e63279fa45048b9e4014c8ea7ba09ae7562
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2015-07-30 23:31:16 +0200

    Tests for running gists from URL

--- tests/test_gists/test_run.py
+++ tests/test_gists/test_run.py
@@ -3,48 +3,107 @@ Tests for the logic of running gists.
 """
 import shlex
 
+from furl import furl
 import mock
+import requests
 from taipan.testing import TestCase
 
 from gisht import BIN_DIR
 import gisht.gists.run as __unit__
 
 
-# TODO(xion): write tests for running gists by URL
-
-
 EXTENSION = '.huh'
 INTERPRETER = 'wat'
 INTERPRETER_ARGV = INTERPRETER + ' %(script)s -- %(args)s'
 
-GIST = 'JohnDoe/foo' + EXTENSION
+OWNER = 'JohnDoe'
+NAME = 'foo' + EXTENSION
+GIST = '%s/%s' % (OWNER, NAME)
+
+ARGS = ('a', 'bc')
+
+
+@mock.patch.object(__unit__, 'ensure_gist', new=mock.Mock())
+class RunGistUrl(TestCase):
+    GIST_ID = '1a2s3d4f5g6h7j8k9l'
+
+    def test_invalid__wrong_host(self):
+        gist = furl('http://www.example.com')
+        with self.assertRaises(SystemExit):
+            __unit__.run_gist_url(gist)
+
+    def test_invalid__malformed_path(self):
+        gist = furl('http://' + __unit__.GITHUB_GISTS_HOST + '/foo')
+        with self.assertRaises(SystemExit):
+            __unit__.run_gist_url(gist)
+
+    @mock.patch.object(__unit__, 'get_gist_info')
+    def test_invalid__failed_gist(self, mock_get_gist_info):
+        mock_get_gist_info.side_effect = requests.exceptions.HTTPError()
+
+        gist = self._url(OWNER, self.GIST_ID)
+        with self.assertRaises(SystemExit):
+            __unit__.run_gist_url(gist)
+
+        mock_get_gist_info.assert_called_once_with(self.GIST_ID)
+
+    @mock.patch.object(__unit__, 'run_named_gist')
+    @mock.patch.object(__unit__, 'get_gist_info')
+    def test_success__no_args(self, mock_get_gist_info, mock_run_named_gist):
+        mock_get_gist_info.return_value = {
+            'owner' : {'login': OWNER},
+            'files': {NAME: '__unused__'},
+        }
+
+        gist = self._url(OWNER, self.GIST_ID)
+        __unit__.run_gist_url(gist)
+
+        mock_run_named_gist.assert_called_once_with(GIST, ())
+
+    @mock.patch.object(__unit__, 'run_named_gist')
+    @mock.patch.object(__unit__, 'get_gist_info')
+    def test_success__with_args(self, mock_get_gist_info, mock_run_named_gist):
+        mock_get_gist_info.return_value = {
+            'owner' : {'login': OWNER},
+            'files': {NAME: '__unused__'},
+        }
+
+        gist = self._url(OWNER, self.GIST_ID)
+        __unit__.run_gist_url(gist, ARGS)
+
+        mock_run_named_gist.assert_called_once_with(GIST, ARGS)
+
+    # Utility functions
+
+    def _url(self, owner, gist_id):
+        return furl('http://%s/%s/%s' % (__unit__.GITHUB_GISTS_HOST,
+                                         owner, gist_id))
 
 
 @mock.patch.dict(__unit__.COMMON_INTERPRETERS, {EXTENSION: INTERPRETER_ARGV})
-class RunGist(TestCase):
+class RunNamedGist(TestCase):
     EXECUTABLE = BIN_DIR / GIST
-    ARGS = ('a', 'bc')
 
     @mock.patch('os.execv')
     def test_direct__no_args(self, mock_execv):
-        __unit__.run_gist(GIST)
+        __unit__.run_named_gist(GIST)
 
         executable = bytes(self.EXECUTABLE)
         mock_execv.assert_called_once_with(executable, [executable])
 
     @mock.patch('os.execv')
     def test_direct__with_args(self, mock_execv):
-        __unit__.run_gist(GIST, self.ARGS)
+        __unit__.run_named_gist(GIST, ARGS)
 
         executable = bytes(self.EXECUTABLE)
         mock_execv.assert_called_once_with(
-            executable, [executable] + list(self.ARGS))
+            executable, [executable] + list(ARGS))
 
     @mock.patch('os.execvp')
     @mock.patch('os.execv')
     def test_via_interpreter__known__no_args(self, mock_execv, mock_execvp):
         mock_execv.side_effect = self._exec_format_error()
-        __unit__.run_gist(GIST)
+        __unit__.run_named_gist(GIST)
 
         argv = shlex.split(INTERPRETER_ARGV % dict(script=self.EXECUTABLE,
                                                    args=''))
@@ -54,10 +113,10 @@ class RunGist(TestCase):
     @mock.patch('os.execv')
     def test_via_interpreter__known__with_args(self, mock_execv, mock_execvp):
         mock_execv.side_effect = self._exec_format_error()
-        __unit__.run_gist(GIST, self.ARGS)
+        __unit__.run_named_gist(GIST, ARGS)
 
         argv = shlex.split(INTERPRETER_ARGV % dict(script=self.EXECUTABLE,
-                                                   args=' '.join(self.ARGS)))
+                                                   args=' '.join(ARGS)))
         mock_execvp.assert_called_once_with(INTERPRETER, argv)
 
     @mock.patch('os.execv')
@@ -66,7 +125,7 @@ class RunGist(TestCase):
 
         gist = GIST.replace(EXTENSION, '.unknown')
         with self.assertRaises(SystemExit):
-            __unit__.run_gist(gist)
+            __unit__.run_named_gist(gist)
 
     @mock.patch('os.execv')
     def test_via_interpreter__no_extension(self, mock_execv):
@@ -74,7 +133,7 @@ class RunGist(TestCase):
 
         gist = GIST.replace(EXTENSION, '')
         with self.assertRaises(SystemExit):
-            __unit__.run_gist(gist)
+            __unit__.run_named_gist(gist)
 
     # Utility functions
 
